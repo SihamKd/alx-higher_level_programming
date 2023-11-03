f9333dca16743e35f3677d5775e4e6f42da11b0b
Siham <khaddou.siham@gmail.com>
2023-11-03 08:12:31 -0700

    update

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri
+"""
+from requests import get
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    response = get(url)
+    bytes_content = response.text
+    string = 'Body response:\n\t- type: {}\n\t- content: {}'.format(
+             type(bytes_content), bytes_content)
+    print(string)
